a408b81b086c1c889f310f0ade8fde03c6f2b8ae
jenwit-dev <johnny.js19@gmail.com>
2025-05-21 02:31:30 +0700

    test3

--- secret/main.js
+++ secret/main.js
@@ -1,13 +1,16 @@
-import fs, { readFile } from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import fs, { readFile } from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 const writeFile = (fileName, data) =>
-    fs.writeFile(fileName, JSON.stringify(data), 'utf-8', (err, data) => console.log(data));
+  fs.writeFile(fileName, JSON.stringify(data), "utf-8", (err, data) =>
+    console.log(data)
+  );
 
-readFile('todo.json', 'utf-8', (err, data) => {
-    let todos = JSON.parse(data);
+readFile("todo.json", "utf-8", (err, data) => {
+  let todos = JSON.parse(data);
 
-    const newTodo = todos.map((todoOb) => ({ ...todoOb, id: uuidv4() }));
+  const newTodo = todos.map((todoOb) => ({ ...todoOb, id: uuidv4() }));
 
-    writeFile('newTodo.json', newTodo);
+  writeFile("newTodo.json", newTodo);
 });
+// sample
